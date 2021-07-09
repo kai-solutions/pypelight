6848bdf4db80d11cf0b94ab5495f8b1c8f7e7b14
kai-solutions <kai-solutions@protonmail.com>
2021-07-09 15:51:21 -0600

    Correct bugs and add new folders

--- pypelight/core/path_setup.py
+++ pypelight/core/path_setup.py
@@ -1,8 +1,13 @@
-import os, re, fnmatch, getpass, shutil
+import os
+import re
+import fnmatch
+import getpass
+import shutil
 
-class Path():
 
-    def __init__(self):
+class Path(object):
+
+    def __init__(self, levels=2):
         self.root_path = os.path.abspath(os.sep)
         self.current_path = os.getcwd()
         self.excludes = [
@@ -11,39 +16,45 @@ class Path():
             os.path.join(self.root_path, 'Users\\Public'),
             os.path.join(self.root_path, 'Users', getpass.getuser(), 'AppData')
         ]
-        self.excludes = r'|'.join([fnmatch.translate(x) for x in self.excludes]) or r'$.'
+        self.excludes = r'|'.join([fnmatch.translate(x)
+                                  for x in self.excludes]) or r'$.'
+        self.levels = levels
+        self.project_root()
+        self.setup_project_path()
 
-    def project_root(self, levels = 1):
-        if levels <= 2:
-            level_map = '.' * levels
+    def project_root(self):
+        if self.levels <= 2:
+            level_map = '.' * self.levels
         else:
-            level_map = ('../' * (levels -1))[:-1]
-        self.project_path = os.path.abspath(os.path.join(self.current_path, level_map))
+            level_map = ('../' * (self.levels - 1))[:-1]
+        self.project_path = os.path.abspath(
+            os.path.join(self.current_path, level_map))
+        return self.project_path
 
-    def walklevel(self, some_dir, level = 1):
+    def walklevel(self, some_dir):
         some_dir = some_dir.rstrip(os.path.sep)
         assert os.path.isdir(some_dir)
         num_sep = some_dir.count(os.path.sep)
         for root, dirs, files in os.walk(some_dir):
             yield root, dirs, files
             num_sep_this = root.count(os.path.sep)
-            if num_sep + level <= num_sep_this:
+            if num_sep + self.levels <= num_sep_this:
                 del dirs[:]
-    
-    def setup_project_path(self, cookiecutter_project=None, levels = 1):
-        if cookiecutter_project is None:
-            cookiecutter_project = self.project_root(levels=levels)
 
-        for root, dirs, files in self.walklevel(cookiecutter_project, 1):
+    def setup_project_path(self, cookiecutter_project=None):
+        if isinstance(cookiecutter_project, type(None)):
+            cookiecutter_project = self.project_root()
+        for root, dirs, files in self.walklevel(cookiecutter_project):
             if os.path.basename(root) == 'data':
                 try:
                     self.edataDir, self.idataDir, self.pdataDir, self.rdataDir = [
                         os.path.join(root, _) for _ in dirs
                     ]
                 except ValueError:
-                    unknown_folder = [_ for _ in dirs in _ not in ('external', 'interim', 'processed', 'raw')]
+                    unknown_folder = [_ for _ in dirs in _ not in (
+                        'external', 'interim', 'processed', 'raw')]
                     print(f'Mapping error for {unknown_folder}')
-                    self.edataDir, self.idataDir, self.pdataDir, self.rdataDir = [
+                    self.edatadir, self.idatadir, self.pdatadir, self.rdatadir = [
                         os.path.join(root, _) for _ in dirs if _ in ('external', 'interim', 'processed', 'raw')
                     ]
                 else:
@@ -51,13 +62,17 @@ class Path():
                 finally:
                     pass
             elif os.path.basename(root) == 'notebooks':
-                self.notebookDir = root
+                self.notebookdir = root
             elif os.path.basename(root) == 'reports':
-                self.reportDir = root
+                self.reportdir = root
             elif os.path.basename(root) == 'src':
-                self.srcDir = root
-    
-    def create_path(path_to_create, delete_first = True, sub_tree = None):
+                self.srcdir = root
+            elif os.path.basename(root) == 'sql':
+                self.sqldir = root
+            elif os.path.basename(root) == 'tests':
+                self.testsdir = root
+
+    def create_path(path_to_create, delete_first=True, sub_tree=None):
         ''' Creates necessary folder paths in where to export files
         '''
         if delete_first:
